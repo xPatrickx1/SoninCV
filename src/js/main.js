52f03e3293fd96c56f189518222dc6cfcd87beff
 Patrick <patrick@MacBook-Pro15-Patrick.local>
2017-11-30 21:22:58 +0200

    logo done

--- src/js/main.js
+++ src/js/main.js
@@ -9,4 +9,21 @@ $(document).ready(function() {
         })
     })();
 
+    $('.blink').on('click', function () {
+        $('.col1').animate({
+            backgraund: 'red'
+        }, 2).delay(0).animate({
+            backgraund: 'green'
+        });
+        $('.bottom').animate({
+            bottom: '200'
+        }, 2).delay(0).animate({
+            bottom: '-300'
+        });
+    });
+
+    $("#navbar_link").click(function() {
+        $('.transform').toggleClass('transform-active');
+    });
+
 });
