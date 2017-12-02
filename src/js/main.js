143b5ec2c0a0eb422e4a34521a7edea8ad00ffd0
 Patrick <patrick@MacBook-Pro15-Patrick.local>
2017-12-02 17:03:24 +0200

    animation+loader

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
+
+
     $('button').click(function() {
         $(this).toggleClass('expanded').siblings('div').slideToggle();
     });
+
 
     (function () {
         $('.hamburger-menu').on('click', function() {
@@ -9,21 +12,27 @@ $(document).ready(function() {
         })
     })();
 
-    $('.blink').on('click', function () {
-        $('.col1').animate({
-            backgraund: 'red'
-        }, 2).delay(0).animate({
-            backgraund: 'green'
-        });
-        $('.bottom').animate({
-            bottom: '200'
-        }, 2).delay(0).animate({
-            bottom: '-300'
+
+    $(window).on('load', function () {
+        var $preloader = $('.loader_box'),
+            $spinner   = $preloader.find('.line-1 .double-bounce2');
+        $spinner.fadeOut();
+        $preloader.delay(1500).fadeOut('slow');
+    });
+
+
+    $(window).scroll(function() {
+        $('.mov').each(function(){
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+screen.height+300) {
+                $(this).addClass('animation');
+            }
+            if (imagePos > topOfWindow+screen.height) {
+                $(this).removeClass('animation');
+            }
         });
     });
 
-    $("#navbar_link").click(function() {
-        $('.transform').toggleClass('transform-active');
-    });
 
 });
