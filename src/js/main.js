719ccd1420c1788458dbe214aa8c78145f48d04f
 Patrick <patrick@MacBook-Pro15-Patrick.local>
2018-01-24 17:00:36 +0200

    secPAnim

--- src/js/main.js
+++ src/js/main.js
@@ -13,34 +13,30 @@ $(document).ready(function() {
     })();
 
 
-    /*$(window).on('load', function () {
+    $(window).on('load', function () {
         var $preloader = $('.loader_box'),
             $spinner   = $preloader.find('.line-1 .double-bounce2');
         $spinner.fadeOut();
         $preloader.delay(1500).fadeOut('slow');
-    });*/
-
+    });
 
     $(window).scroll(function() {
         $('.mov').each(function(){
             var imagePos = $(this).offset().top;
             var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow+screen.height+300) {
+            if (imagePos < topOfWindow+30) {
                 $(this).addClass('animation');
-            }
-            if (imagePos > topOfWindow+screen.height) {
-                $(this).removeClass('animation');
             }
         });
     });
+
 
 
     $('#fullpage').fullpage({
         menu: '#menu',
         anchors:['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage'],
         navigation: true,
-        fixedElements: '#header, .footer',
-
+        fixedElements: '#header, .footer'
     });
 
 
